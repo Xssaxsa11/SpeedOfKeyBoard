bd153761fa9cdf2bb6e06fbf62ea65e91a6c0659
Влад Подрез <podreze33@gmail.com>
2023-06-08 17:39:03 +0300

    Добавьте файлы проекта.

---
+++ SpeedOfKeyBoard/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SpeedOfKeyBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool helper = true;
+            while (helper)
+            {
+                Console.WriteLine("нажмите q чтобы начать");
+                string c = Console.ReadLine();
+                if(c.ToLower() == "q")
+                {
+                    var firsttime = DateTime.Now;
+                    string words = Console.ReadLine();
+                    var secondtime = DateTime.Now;
+                    var diff = secondtime.Subtract(firsttime);
+                    double secsdiff = (double)diff.TotalSeconds;
+                    string[] check = words.Split(',', '.', ' ', '!', '!', '?');
+                    Console.WriteLine($"Вы ввели {check.Length} слов за {Math.Round(secsdiff,2)} секунд");
+                    
+                }
+            }
+
+        }
+    }
+}
